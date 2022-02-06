d1eaeb0510e90932148174cf576604a94390ade0
wa5 <waseemslyar@gmail.com>
2022-02-06 14:07:27 +0530

    aaAdag

--- fullstck/frontend/languages/typescripses/dataypes/default/object/object.js
+++ fullstck/frontend/languages/typescripses/dataypes/default/object/object.js
@@ -1,21 +1,7 @@
-var OBJ = {};
-//var OBJ:{[key:string]:any}={}
-//or
-var OBJ2 = {};
-OBJ2.jj = "dhg";
-OBJ.o = "jjj";
-console.log(OBJ2);
-//it gives errors
-var cahir4 = {
-    legs: 4,
-    color: "red",
-    company: { wood: { rowwood: 'uuu' }, nails: 'yy' }
-};
-// console.log(cahir4.legs)
 var cahir = {
     legs: 4,
     color: "red",
     company: { wood: { rowwood: 'uuu' }, nails: 'yy' }
 };
-cahir.waite = 76;
-console.log(cahir);
+console.log(cahir === null || cahir === void 0 ? void 0 : cahir.legs);
+
